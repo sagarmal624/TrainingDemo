ae3fc49e50f647eb44112608aec0febceb285ba8
sagarmal624 <sagarmal624@gmail.com>
2018-12-26 08:57:15 +0530

    fixed tomcat issue

--- src/main/java/com/sagarandcompany/dockerdemo/DockerdemoApplication.java
+++ src/main/java/com/sagarandcompany/dockerdemo/DockerdemoApplication.java
@@ -2,6 +2,7 @@ package com.sagarandcompany.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
@@ -10,6 +11,11 @@ public class DockerdemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DockerdemoApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder
+                                                         application) {
+        return application.sources(DockerdemoApplication.class);
     }
 //
 //    @Bean
